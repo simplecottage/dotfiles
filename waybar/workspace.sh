62dc7234331641920da573170be1758a3cb0e154
simplecottage <simplecottage@proton.me>
2025-01-20 20:55:54 +0100

    - script

--- waybar/workspace.sh
+++
@@ -1,8 +0,0 @@
-#!/bin/bash
-
-# fetch workspace info from hyprctl
-workspaces=$(hyprctl workspaces -j | jq -r '.[] | "\(.id):\(.name) \(.windows > 0 and "*" or "")"')
-
-# output formatted workspaces
-echo "$workspaces" | tr '\n' ' '
-
